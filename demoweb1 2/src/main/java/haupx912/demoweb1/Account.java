387cfded3bfb188cc4b46ed1292e5f8147753ade
c200610 <c200610@ccg.ac.jp>
2022-01-06 22:00:42 +0900

    master

--- demoweb1 2/src/main/java/haupx912/demoweb1/Account.java
+++ demoweb1 2/src/main/java/haupx912/demoweb1/Account.java
@@ -2,18 +2,20 @@ package haupx912.demoweb1;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-
+import javax.validation.constraints.Email;
 
 import lombok.Data;
-
 @Data
 @Entity
 public class Account {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    @Email
+    private String password;
     private Long age;
     private String tel;
     private String address;
@@ -26,15 +28,23 @@ public class Account {
     }
 
 
-    public Account(String name, Long age, String tel, String address, boolean gender, String schools) {
-        super();
-        this.name = name;
+
+
+
+    public Account(String firstName, String lastName, String userName, String password, Long age, String tel,
+            String address, boolean gender, String schools) {
+                super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
         this.age = age;
         this.tel = tel;
         this.address = address;
         this.gender = gender;
         this.schools = schools;
     }
+
 
     
 }
