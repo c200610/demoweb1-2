6bbad2befc80e138e37fe08cfb551e7e1c7672d0
c200610 <c200610@ccg.ac.jp>
2022-01-16 00:24:09 +0900

    master

--- demoweb1 2/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ACCOUNT(first_name, last_name, user_name, password, age, tel,address ,gender,schools) VALUES ('Nguyen','Hau','Admin','Admin123',26, '07088888888', '東京都千代田区神田小川町1-1-1',true,'中央情報専門学校');
-INSERT INTO ACCOUNT(first_name,last_name, user_name, password, age, tel, address,gender,schools) VALUES ('鳥塚','亜衣','Aipx912','Aipx9123',24, '07066666666', '東京都千代田区神田小川町1-1-1',false,'東京大学');
