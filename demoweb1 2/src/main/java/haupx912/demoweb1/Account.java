e15479e1e9bda9fa5251b03c3a3493e32042d167
c200610 <c200610@ccg.ac.jp>
2022-01-15 10:23:25 +0900

    master

--- demoweb1 2/src/main/java/haupx912/demoweb1/Account.java
+++ demoweb1 2/src/main/java/haupx912/demoweb1/Account.java
@@ -1,25 +1,45 @@
 package haupx912.demoweb1;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 @Data
 @Entity
+@Table(name = "Account", uniqueConstraints = @UniqueConstraint(columnNames = "user_name"))
 public class Account {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
+
     private String lastName;
+    @Column(name = "user_name")
+
     private String userName;
+    @Column(name = "password")
+
     private String password;
+    @Column(name = "age")
+
     private Long age;
+    @Column(name = "tel")
+
     private String tel;
+    @Column(name = "adress")
+
     private String address;
+    @Column(name = "gender")
+
     private boolean gender;
+    @Column(name = "schools")
+
     private String schools;
 
     
